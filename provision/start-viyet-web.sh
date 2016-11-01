560c078da2a29725dc167bf5491a53b7ebf15177
Lucas <lucas@vanstaden.com.au>
2016-11-01 17:27:28 +0800

    updates to viyet

--- provision/start-viyet-web.sh
+++ provision/start-viyet-web.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 NAME=`hostname`
-BASEFOLDER=/vagrant/machines/$NAME/www/
+BASEFOLDER=/vagrant/machines/viyet/www/
 DOCROOT=$BASEFOLDER/docroot
-
-
 
 if [ ! -f $DOCROOT/app/etc/local.xml ]; then
     ln -s $DOCROOT/app/etc/local.xml.dev $DOCROOT/app/etc/local.xml
@@ -14,13 +12,12 @@ cat >/etc/nginx/sites-available/default <<'EOL'
 server {
   listen 80 default_server;
 
-  server_name *.viyet.biz viyet.biz;
+  server_name *.viyet.proxiblue.dev viyet.proxiblue.dev;
 
   allow all;
 
   root /vagrant/machines/viyet/www/docroot/www/;
   index index.php;
-  server_name viyet.biz;
   access_log /var/log/nginx/access.log;
   error_log /var/log/nginx/error.log;
 
@@ -32,33 +29,19 @@ server {
     expires max;
   }
 
-  #location / {
-  #  index index.html index.php;
-  #  try_files $uri $uri/ @handler;
-  #  expires 30d;
-  #}
-
-  location ^~ /app/ { deny all; }
-  location ^~ /includes/ { deny all; }
-  location ^~ /lib/ { deny all; }
-  location ^~ /media/downloadable/ { deny all; }
-  location ^~ /pkginfo/ { deny all; }
-  location ^~ /report/config.xml { deny all; }
-  location ^~ /var/ { deny all; }
-
-  location /var/export/ {
-    auth_basic "Restricted";
-    auth_basic_user_file htpasswd;
-    autoindex on;
+  location / {
+    index index.html index.php;
+    try_files $uri $uri/ @handler;
+    expires 30d;
   }
 
   location  /. {
     return 404;
   }
 
-  #location @handler {
-  #  rewrite / /index.php;
-  #}
+  location @handler {
+    rewrite / /index.php;
+  }
 
   location ~ .php/ {
     fastcgi_read_timeout 1800;
@@ -96,12 +79,6 @@ server {
 
   location ~* (.+)\.(\d+)\.(js|css|png|jpg|jpeg|gif)$ {
     try_files $uri $1.$3;
-  }
-
-  #webfonts cross-domain
-
-  location ~* \.(ttf|ttc|otf|eot|woff|svg|font.css)$ {
-    add_header Access-Control-Allow-Origin *;
   }
 
 }
