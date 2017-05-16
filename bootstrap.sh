aa367c19bbd61f9faaf97e3f5788d5026cf4c573
Digital Ocean Droplet <sales@proxiblue.com.au>
2017-05-16 15:36:06 +0000

    adjustments crated on droplet

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 /etc/init.d/mysqld start
+
+mysql -uroot <<MYSQL_SCRIPT
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT ALL PRIVILEGES ON *.* TO 'dev'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
 cp -xav /tmp/magento_nginx/* /etc/nginx/
 chown root:root /etc/nginx/ -R
 #cp -xav /tmp/www.conf /etc/php-fpm.d/www.conf
 
 /etc/init.d/php54-php-fpm start
 service nginx restart
+
+curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
+apt-get install --yes nodejs
 
 cd /vagrant/sites/ntotank
 if [ -e ./composer.json ] ; then
@@ -18,13 +28,17 @@ if [ ! -L ./local.xml ] ; then
         ln -s ./local.xml.dev ./local.xml
         n98-magerun db:create
 fi
+mkdir /vagrant/sites/ntotank/snapshot
+chmod 777 /vagrant/sites/ntotank/snapshot
 
 cd /vagrant/sites/protank
 if [ -e ./composer.json ] ; then
     composer update
 fi
-npm install
+npm install 
+npm install gulp -g
 npm rebuild node-sass
+
 /bin/bash buildGulp.sh
 dos2unix /vagrant/sites/protank/shell/*.sh
 cd app/etc
@@ -32,6 +46,9 @@ if [ ! -L ./local.xml ] ; then
         ln -s ./local.xml.dev ./local.xml
         n98-magerun db:create
 fi
+mkdir /vagrant/sites/protank/snapshot
+chmod 777 /vagrant/sites/protank/snapshot
+
 cd /vagrant/sites/sprayersupplies
 if [ -e ./composer.json ] ; then
     composer update
@@ -42,6 +59,9 @@ if [ ! -L ./local.xml ] ; then
         ln -s ./local.xml.dev ./local.xml
         n98-magerun db:create
 fi
+mkdir /vagrant/sites/sprayersupplies/snapshot
+chmod 777 /vagrant/sites/sprayersupplies/snapshot
+
 cd /vagrant/sites/pvcpipesupplies
 if [ -e ./composer.json ] ; then
     composer update
@@ -52,6 +72,8 @@ if [ ! -L ./local.xml ] ; then
         ln -s ./local.xml.dev ./local.xml
         n98-magerun db:create
 fi
+mkdir /vagrant/sites/pvcpipesupplies/snapshot
+chmod 777 /vagrant/sites/pvcpipesupplies/snapshot
 
 echo "cd /vagrant/sites" >> /home/vagrant/.bashrc
 
