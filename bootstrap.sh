a07d34282a4b66e42234858a7345b332af9b27d6
Digital Ocean Droplet <sales@proxiblue.com.au>
2017-05-17 03:01:27 +0000

    add in basic auth and inits

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,9 @@ cp -xav /tmp/magento_nginx/* /etc/nginx/
 chown root:root /etc/nginx/ -R
 cp -xav /tmp/www.conf /etc/php5/fpm/pool.d/
 
+chown nginx:vagrant /vagrant/sites -R
+chmod 775 /vagrant/sites -R
+
 /etc/init.d/php54-php-fpm restart
 service nginx restart
 
@@ -28,6 +31,8 @@ if [ ! -L ./local.xml ] ; then
 fi
 mkdir /vagrant/sites/ntotank/snapshot
 chmod 777 /vagrant/sites/ntotank/snapshot
+mkdir /vagrant/sites/ntotank/var
+chmod 777 /vagrant/sites/ntotank/var
 
 cd /vagrant/sites/protank
 if [ -e ./composer.json ] ; then
@@ -46,6 +51,8 @@ if [ ! -L ./local.xml ] ; then
 fi
 mkdir /vagrant/sites/protank/snapshot
 chmod 777 /vagrant/sites/protank/snapshot
+mkdir /vagrant/sites/protank/var
+chmod 777 /vagrant/sites/protank/var
 
 cd /vagrant/sites/sprayersupplies
 if [ -e ./composer.json ] ; then
@@ -59,6 +66,8 @@ if [ ! -L ./local.xml ] ; then
 fi
 mkdir /vagrant/sites/sprayersupplies/snapshot
 chmod 777 /vagrant/sites/sprayersupplies/snapshot
+mkdir /vagrant/sites/sprayersupplies/var
+chmod 777 /vagrant/sites/sprayersupplies/var
 
 cd /vagrant/sites/pvcpipesupplies
 if [ -e ./composer.json ] ; then
@@ -72,6 +81,9 @@ if [ ! -L ./local.xml ] ; then
 fi
 mkdir /vagrant/sites/pvcpipesupplies/snapshot
 chmod 777 /vagrant/sites/pvcpipesupplies/snapshot
+
+mkdir /vagrant/sites/pvcpipesupplies/var
+chmod 777 /vagrant/sites/pvcpipesupplies/var
 
 echo "cd /vagrant/sites" >> /home/vagrant/.bashrc
 echo "use: 'vagrant ssh' to ssh into server without a password"
