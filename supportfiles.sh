60a0002f7ad1b1579791f78602ae214a3d776017
Lucas <lucas.vanstaden@enjo.com.au>
2019-11-14 22:08:12 +0800

    rework environment to use hostmanager

--- supportfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d /home/vagrant/.n98-magerun/modules ]; then
-    mkdir -p /home/vagrant/.n98-magerun/modules/
-    mkdir /home/vagrant/.n98-magerun/modules/
-    cd /home/vagrant/.n98-magerun/modules/
-    git clone https://github.com/peterjaap/magerun-addons.git pj-addons
-    chown vagrant:vagrant /home/vagrant/.n98-magerun -R
-fi
